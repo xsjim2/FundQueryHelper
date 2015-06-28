4aa3193d3316a529cf4d2bdd1ca739ff6b38fa9c
xsjim2 <xsjim2@163.com>
2015-06-28 16:21:45 +0800

    Initial commit

---
+++ FundQueryHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FundQueryHelper
+//
+//  Created by wangminhong on 15/6/28.
+//  Copyright (c) 2015年 wangmh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FundQueryHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FundQueryHelper
+//
+//  Created by wangminhong on 15/6/28.
+//  Copyright (c) 2015年 wangmh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
